be6f076316bfde33394d94b3dfa948504c3394ee
Rafael Vale <rafael.marques.vale.dev@gmail.com>
2020-05-14 23:24:01 -0300

    criação de agendamentos

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -17,6 +17,7 @@ describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
+      user_id: '123123',
       provider_id: '123123',
     });
 
@@ -30,12 +31,14 @@ it('should not be able to create two appointments on the same time', async () =>
 
   await createAppointment.execute({
     date: appointmentDate,
+    user_id: '123123',
     provider_id: '123123',
   });
 
   expect(
     createAppointment.execute({
       date: appointmentDate,
+      user_id: '123123',
       provider_id: '123123',
     }),
   ).rejects.toBeInstanceOf(AppError);
