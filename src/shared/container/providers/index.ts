396eae6de4c60c479be6ea75461a305449d9b535
Rafael Vale <rafael.marques.vale.dev@gmail.com>
2020-05-18 16:09:46 -0300

    Upload com Amazon S3

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,24 +1,3 @@
-import { container } from 'tsyringe';
-
-import IStorageProvider from './StorageProvider/models/IStorageProvider';
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
-
-import IMailProvider from './MailProvider/models/IMailProvider';
-import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
-
-import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
-import HandlebarMailTemplateProvider from './MailTemplateProvider/implementations/HandleBarsMailTemplateProvider';
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-);
-
-container.registerSingleton<IMailTemplateProvider>(
-  'MailTemplateProvider',
-  HandlebarMailTemplateProvider,
-);
-container.registerInstance<IMailProvider>(
-  'MailProvider',
-  container.resolve(EtherealMailProvider),
-);
+import './StorageProvider';
+import './MailTemplateProvider';
+import './MailProvider';
